be1f4fb03f70190ee763070917d18f6b10e6a32c
Etai Koren <kodkod@kodkod.org>
2012-10-02 02:51:53 +0200

    added Google Analytics

--- index.php
+++ index.php
@@ -190,6 +190,7 @@
     <script src="/js/jtimer.js"></script>
 
     <script type="text/javascript">
+    
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-35247202-1']);
     _gaq.push(['_trackPageview']);
