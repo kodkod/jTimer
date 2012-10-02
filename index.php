1875806f3310f3562f1160c9eece33b3ca59f248
Etai Koren <kodkod@kodkod.org>
2012-10-02 02:44:23 +0200

    added GoogleAnalytics

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>LocalStorage Clients Time Manager</title>
+    <title>jTimer - Javascript LocalStorage Work Timer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -40,7 +40,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-          <a class="brand" href="/">JS Time Manager</a>
+          <a class="brand" href="/">jTimer</a>
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
@@ -187,9 +187,20 @@
     <script src="/js/jquery-1.8.2.min.js"></script>
     <script src="/js/jquery.colorbox-min.js"></script>
     <script src="/js/bootstrap.js"></script>
-    <script src="/js/jtimer.js">
-		
-    </script>
-    
+    <script src="/js/jtimer.js"></script>
+
+    <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-35247202-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>    
   </body>
 </html>
