0b73e2f1fee79a5215891952d304069ab62cb262
Etai Koren <kodkod@kodkod.org>
2012-10-07 16:43:53 +0200

    Added github Ribbon

--- about.php
+++ about.php
@@ -31,7 +31,7 @@
   </head>
 
   <body>
-	<a id='forkme' href="https://github.com/you"><img style="position: absolute; top: 40px; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+	<a id='forkme' href="https://github.com/kodkod/jTimer"><img style="position: absolute; top: 40px; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
